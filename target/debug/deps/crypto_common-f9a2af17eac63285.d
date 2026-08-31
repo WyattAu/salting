1fd471689fb6a7bc7856aadea3cf33581ef3bf30
WyattAu <wyatt_au@protonmail.com>
2026-08-31 10:47:50 +0100

    feat: Phase 0+1 — security fixes, tests, real implementations

--- target/debug/deps/crypto_common-f9a2af17eac63285.d
+++
@@ -1,5 +0,0 @@
-/home/wyatt/dev/src/github.com/WyattAu/salting/target/debug/deps/crypto_common-f9a2af17eac63285.d: /home/wyatt/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crypto-common-0.1.7/src/lib.rs
-
-/home/wyatt/dev/src/github.com/WyattAu/salting/target/debug/deps/libcrypto_common-f9a2af17eac63285.rmeta: /home/wyatt/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crypto-common-0.1.7/src/lib.rs
-
-/home/wyatt/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crypto-common-0.1.7/src/lib.rs:
